79e6335428b5a78c2840b9eaa5e60caf751f5f22
PandyaDeval <dev23199@yahoo.in>
2019-02-03 07:57:30 +0530

    Checkpoint Progress

--- nlp.py
+++ nlp.py
@@ -10,7 +10,7 @@ import re, time, math
 import spacy
 import speech_recognition as sr
 import pyaudio
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import PIL
 import cv2
 import matplotlib.pyplot as plt
@@ -23,8 +23,13 @@ pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tessera
 frequency = 1500
 duration = 1000
 
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype('Roboto-Bold.ttf', size=10)
 nlp=spacy.load('en') 
-
+nlp.vocab["go"].is_stop = False
+nlp.vocab["back"].is_stop = False
+nlp.vocab["all"].is_stop = False
+#print(nlp.Defaults.stop_words)
 def noun(x):
     nounlist=[]
     for token in x:
@@ -90,7 +95,7 @@ def user_double_click(text):
     data=pytesseract.image_to_data(img)
 
     data=data.split()
-    #print(data)
+    print(data)
     
     for i in range(len(data)):
         if ' '.join(text) in data[i].lower():
@@ -102,6 +107,31 @@ def user_double_click(text):
             mouse.double_click(button="left",coords=(coord_x,coord_y))
             print("Double Clicked at: (" ,coord_x,",", coord_y,")")
             break
+        
+def user_right_click(text):
+    time.sleep(0.5)
+    img = PIL.ImageGrab.grab()
+    #imgplot = plt.imshow(img)
+    data=pytesseract.image_to_data(img)
+
+    data=data.split()
+    print(data)
+    
+    for i in range(len(data)):
+        if ' '.join(text) in data[i].lower():
+            print("Match Found")
+            #coord_x = int(data[i-5]) + int(data[i-3]/2)
+            #coord_y = int(data[i-4]) + int(data[i-2]/2)
+            coord_x = int(data[i-5]) + int(int(data[i-3])/2)
+            coord_y = int(data[i-4]) + int(int(data[i-2])/2)
+            mouse.click(button="right",coords=(coord_x,coord_y))
+            print("Right Clicked at: (" ,coord_x,",", coord_y,")")
+            break
+        
+def keyboard_ip(text):
+    for x in text:
+        SendKeys(x)
+        SendKeys("{VK_SPACE}")
         
 def wait(text):
     for i in range(len(text)):
@@ -116,7 +146,8 @@ while True:
     #time.sleep(1)
     query = str(speech.press_record())       #converting speech to text using speech_to_text API 
     #query=input("Enter Command (Type exit to quit): ")
-    if query.lower()=='exit':
+    #time.sleep(3)
+    if query.lower()=='exit' or query.lower()=='quit':
         break
     else:
         print(query) 
@@ -136,7 +167,6 @@ while True:
         pass
     else:
         do=verb(doc)[0]
-    
     words=[word.text.lower() for word in doc if word.is_stop==False]
     words=list(words)
     #verbs, obj = words[0], ' '.join(words[1:])
@@ -157,12 +187,33 @@ while True:
     #elif float(similarity_click)>0.5:
     elif verbs=='click':
         user_single_click(obj)
-    elif verbs=="double" or verb=='doubleclick':
+    elif verbs=="double" or verbs=='doubleclick':
         user_double_click(obj[1:])
+    elif verbs=='right' or verbs=='rightclick':
+        user_right_click(obj[1:])
+    elif verbs=="type" or verbs=="write":
+        keyboard_ip(obj)
+    elif verbs=="go":
+        if obj==["back"]:
+            SendKeys("%{LEFT}")
+        if obj==["forward"]:
+            SendKeys("%{RIGHT}")
+    elif verbs=="play" or verbs=="pause":
+        SendKeys("{VK_SPACE}")
+    elif verbs=="select" and obj==["all"]:
+        SendKeys("^a")
+    elif verbs=="copy":
+        SendKeys("^c")
+    elif verbs=="paste":
+        SendKeys("^v")
+    elif verbs=="cut":
+        SendKeys("^x")
+    elif verbs=="close":
+        SendKeys("%{F4}")
     elif verbs=='wait':
         wait(obj)
     
-    time.sleep(5)
+    time.sleep(3)
 
 
 #imge = Image.open(img)
